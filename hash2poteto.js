c1151176f342d436bfb9554ab2ad49e84a4aae73
kaeru <83284713+uchuukaeru@users.noreply.github.com>
2025-08-25 10:46:35 +0900

    Create hash2poteto.js

---
+++ hash2poteto.js
@@ -0,0 +1,33 @@
+const regHead = new RegExp("^([0-9a-f]{12})")
+/**
+ * @param {string} data 
+ * @param {number} iw 
+ */
+export const createPoteto = (data, iw) => {
+    const imagelist = new Array(iw);
+    for (let j = 0; j < iw; j++) {
+        imagelist[j] = new Array(iw).fill(255);
+    }
+    const exec = regHead(data);
+    if (exec === null) {
+        return imagelist;
+    }
+    const shapeStr = exec[1];
+
+    Array.prototype.forEach.call(shapeStr, function(s, index) {
+        const binNum = parseInt(s, 16).toString(2).padStart(4, '0');
+        const posi = binNum[0];
+        const shape = parseInt(binNum.slice(-3), 2) !== 0? parseInt(binNum.slice(-3), 2): 1;
+        
+        const column = new Array(iw).fill(255);
+        const shiftNum = (8 - shape) / 2;
+        const startShiftNum = posi === "1"? Math.floor(shiftNum) : Math.ceil(shiftNum);
+
+        for (let j = startShiftNum; j < shape + startShiftNum; j++) {
+            line[4 + j] = 1;
+        }
+        imagelist[2 + index] = line;
+    });
+
+    return imagelist;
+}
